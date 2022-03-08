eb708847bc0f034c2091ec0169f13c2c087f058c
Bruno <hay.bruno1999@gmail.com>
2022-03-07 22:19:12 -0300

    'v20'

--- commands/underDevelopment.py
+++ commands/underDevelopment.py
@@ -14,7 +14,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
 import time
+
 
 
 def initCreds():
@@ -67,6 +69,16 @@ class UnderDevelopment(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 ###########################################
+    @commands.command(name='seleniumTest',help = 'Testing selenium imp')
+    async def seleniumT(self,ctx):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--no-sandbox')
+        driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'),options=chrome_options)
+        driver.get('https://www.dac.unicamp.br/portal/')
+        print(driver.title)
 ##Prepare game room
     @commands.command(name='letsPlay', help = 'Cria salas para certos jogos. \n[game...] é o nome do jogo.')
     async def newRoom(self, ctx, *game):
